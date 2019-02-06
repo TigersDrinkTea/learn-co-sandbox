b7eb475f8ef39e83aacc4675dea6fe600b736a14
brainy-language-7409 <castlesofcode@gmail.com>
2019-02-06 22:46:35 +0000

    Automatically backed up by Learn

---
+++ practice.js
@@ -0,0 +1,12 @@
+
+var a = 5,  b = 2, c = 3, d= 'hello', e = 'goodbye';
+b = 'Dinkleberry'
+
+console.log(b)
+
+
+function speaker() {
+  var sentence = 'Bird is the word.';
+ 
+  console.log(sentence);
+}
